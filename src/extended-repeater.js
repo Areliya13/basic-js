926245cbfc61041961049041b3c424e8a462004b
Areliya13 <sharipovatamara@gmail.com>
2022-09-05 20:59:58 +0300

    feat: completed extended repeater&recursive depth

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options ) {
+  let mainStr = str
+  let addStr = ''
+  if ('addition' in options){
+    addStr = options.addition
+    let addSep
+    'additionSeparator' in options ? addSep = options.additionSeparator : addSep = '|'
+    let addRepT
+    'additionRepeatTimes' in options ? addRepT = options.additionRepeatTimes : addRepT = 0    
+    for (let i = 1; i < addRepT; i++) {
+      addStr = `${addStr}${addSep}${options.addition}`
+    }
+    mainStr = `${mainStr}${addStr}`
+  }
+  let sep
+  'separator' in options ? sep = options.separator : sep = '+'
+  let repT
+  'repeatTimes' in options ? repT = options.repeatTimes : repT = 0    
+  let res = mainStr
+  for (let i = 1; i < repT; i++) {
+    res = `${res}${sep}${mainStr}`
+  }
+  return res
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    // throw new NotImplementedError('Not implemented');
+    // // remove line with error and write your code here
+    let depth = 0
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+      let elDepth = 0
+      if (Array.isArray(element)){
+        elDepth = this.calculateDepth(element)
+        if (elDepth > depth) depth = elDepth
+      }
+    }
+    return depth + 1
   }
 }
 
