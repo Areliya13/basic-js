dc156684e4ec64af15f0ebcb5b8659da9ac07f81
Areliya13 <sharipovatamara@gmail.com>
2022-10-14 22:42:23 +0300

    feat: completed common-characters, dns-stats, encode-line, file-names & vigenere-cipher

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function encodeLine(str) {
+  if (str === ''){
+      return '';
+  }
+  let curSymbol = str[0];
+  let count = 1;
+  let res = '';
+
+  for (let i = 1; i < str.length; i++) {
+      const element = str[i];
+      if (element === curSymbol) {
+          count++;
+      } else {
+          if (count === 1){
+              res = `${res}${curSymbol}`
+          } else {
+              res = `${res}${count}${curSymbol}`
+          }
+          curSymbol = element;
+          count = 1;
+      }
+  }
+  
+  if (count === 1){
+      res = `${res}${curSymbol}`
+  } else {
+      res = `${res}${count}${curSymbol}`
+  }
+  return res;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,111 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+
+  constructor(isDirect){
+    console.log(isDirect);
+    if (isDirect == undefined){
+      this.isDirect = true;
+    } else {
+      this.isDirect = isDirect;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, keystr) {
+      if (typeof(str) !== 'string'){
+          throw new Error('Incorrect arguments!');
+      }
+      if (typeof(keystr) !== 'string'){
+          throw new Error('Incorrect arguments!');
+      }
+
+      const codeA = 'A'.charCodeAt(0);
+      const upperString = str.toUpperCase();
+      const upperKey = keystr.toUpperCase();
+
+      const strArr = [];
+      const keyArr = [];
+      
+      for (let i = 0; i < upperString.length; i++) {
+          strArr.push(upperString.charCodeAt(i) - codeA);
+      }
+
+      for (let i = 0; i < upperKey.length; i++) {
+          keyArr.push(upperKey.charCodeAt(i) - codeA);
+      }
+
+      let iter = 0;
+      const len = keyArr.length;
+      const resArr = []
+
+      for (let i = 0; i < strArr.length; i++) {
+          const element = strArr[i];
+          if ((element >= 0) && (element < 26)){
+              resArr.push((element + keyArr[iter]) % 26);
+              iter = (iter + 1) % len;
+          } else {
+              resArr.push(element);
+          }
+      }
+
+      const res = resArr.reduce((acc, el) => {
+          if (this.isDirect) {
+              return `${acc}${String.fromCharCode(el + codeA)}`;
+          } else {
+              return `${String.fromCharCode(el + codeA)}${acc}`;
+          }
+      }, '')
+
+      return res;
+  }
+
+  decrypt(str, keystr) {
+      if (typeof(str) !== 'string'){
+          throw new Error('Incorrect arguments!');
+      }
+      if (typeof(keystr) !== 'string'){
+          throw new Error('Incorrect arguments!');
+      }
+
+      const codeA = 'A'.charCodeAt(0);
+      const upperString = str.toUpperCase();
+      const upperKey = keystr.toUpperCase();
+
+      const strArr = [];
+      const keyArr = [];
+      
+      for (let i = 0; i < upperString.length; i++) {
+          strArr.push(upperString.charCodeAt(i) - codeA);
+      }
+
+      for (let i = 0; i < upperKey.length; i++) {
+          keyArr.push(upperKey.charCodeAt(i) - codeA);
+      }
+
+      let iter = 0;
+      const len = keyArr.length;
+      const resArr = []
+
+      for (let i = 0; i < strArr.length; i++) {
+          const element = strArr[i];
+          if ((element >= 0) && (element < 26)){
+              resArr.push((26 + element - keyArr[iter]) % 26);
+              iter = (iter + 1) % len;
+          } else {
+              resArr.push(element);
+          }
+      }
+
+      const res = resArr.reduce((acc, el) => {
+          if (this.isDirect) {
+              return `${acc}${String.fromCharCode(el + codeA)}`;
+          } else {
+              return `${String.fromCharCode(el + codeA)}${acc}`;
+          }
+      }, '')
+
+      return res;
   }
 }
 
